a46bb1992cd16e396d37c10644c54189e1fc2c9b
njamhour <njjamhour@hotmail.com>
2018-03-23 22:27:23 -0300

    Verificar projeto

--- VendasOsorioA/Model/ItemVenda.cs
+++ VendasOsorioA/Model/ItemVenda.cs
@@ -8,5 +8,10 @@ namespace VendasOsorioA.Model
 {
     class ItemVenda
     {
+        public Produto Produto { get; set; }
+        public DateTime Date { get; set; }
+        public int Quantidade { get; set; }
+        public double Valor { get; set; }
+
     }
 }
